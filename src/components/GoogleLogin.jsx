b6739f4943d553dcc8b57f3d98c501b4a1997922
lindorapel <71240067+lindorapel@users.noreply.github.com>
2023-10-18 11:28:52 +0700

    Add Google Login (#16)
    * Add Google Login
    
    * resolve conflict

---
+++ src/components/GoogleLogin.jsx
@@ -0,0 +1,60 @@
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+// import { Button } from "react-bootstrap";
+import PropTypes from "prop-types";
+
+function GoogleLogin({ buttonText }) {
+  const registerLoginWithGoogleAction = async (accessToken) => {
+    try {
+      let data = JSON.stringify({
+        access_token: accessToken,
+      });
+
+      let config = {
+        method: "post",
+        maxBodyLength: Infinity,
+        url: `${import.meta.env.VITE_API_URL}/api/v1/auth/google`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: data,
+      };
+
+      const response = await axios.request(config);
+      const { token } = response.data.data;
+
+      localStorage.setItem("token", token);
+
+      // navigate("/");
+
+      // Temporary solution
+      window.location.href = "/";
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        alert(error.response.data.message);
+        return;
+      }
+      alert(error.message);
+    }
+  };
+
+  const loginWithGoogle = useGoogleLogin({
+    onSuccess: (responseGoogle) =>
+      registerLoginWithGoogleAction(responseGoogle.access_token),
+  });
+
+  return (
+    <button
+      className="ms-2 text-white bg-red-600 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center-800"
+      onClick={() => loginWithGoogle()}
+    >
+      {buttonText}
+    </button>
+  );
+}
+
+GoogleLogin.propTypes = {
+  buttonText: PropTypes.string,
+};
+
+export default GoogleLogin;
