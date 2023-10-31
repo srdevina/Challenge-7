1228ee3bac36fb96ef08a4bebfcaa42e391fcc04
HelmiITK <helmi220602@gmail.com>
2023-10-31 19:23:28 +0800

    add redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducers from "./reducers";
+import thunk from "redux-thunk"
 
 // create the store or temporary state database
 export default configureStore({
     reducer: rootReducers,
-    devTools: import.meta.env.MODE === "development"
+    devTools: import.meta.env.MODE === "development",
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk), // redux thunk
 });
