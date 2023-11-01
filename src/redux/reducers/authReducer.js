3edfaa70e1eeff3717aa7117779ab5ea9f3e15b8
HelmiITK <helmi220602@gmail.com>
2023-11-01 21:28:02 +0800

    add register to redux

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -2,30 +2,30 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // initial state
 const initialState = {
-  token: localStorage.getItem("token") || null,
-  user: null,
+    token: localStorage.getItem("token") || null,
+    user: null,
 };
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    setToken: (state, action) => {
-      if (action.payload) {
-        localStorage.setItem("token", action.payload);
-      } else {
-        localStorage.removeItem("token");
-      }
+    name: "auth",
+    initialState,
+    reducers: {
+        setToken: (state, action) => {
+            if (action.payload) {
+                localStorage.setItem("token", action.payload);
+            } else {
+                localStorage.removeItem("token");
+            }
 
-      state.token = action.payload;
+            state.token = action.payload;
+        },
+        setUser: (state, action) => {
+            state.user = action.payload;
+        },
+        setIsRegistered: (state, action) => {
+            state.isRegistered = action.payload;
+        },
     },
-    setUser: (state, action) => {
-      state.user = action.payload;
-    },
-    setIsRegistered: (state, action) => {
-      state.isRegistered = action.payload;
-    },
-  },
 });
 
 export const { setToken, setIsRegistered, setUser } = authSlice.actions;
